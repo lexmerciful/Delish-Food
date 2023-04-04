7ea4c624440a3b2915073afd713fb0a9c0ed73b6
Merciful Afolabi <lexmerciful@gmail.com>
2023-04-04 21:59:27 +0100

    Default NavHost, Save app state and white status bar

--- app/src/main/java/com/lexmerciful/delishfood/viewModel/HomeViewModel.kt
+++ app/src/main/java/com/lexmerciful/delishfood/viewModel/HomeViewModel.kt
@@ -24,12 +24,19 @@ class HomeViewModel(
     private var bottomSheetMealLiveData = MutableLiveData<Meal>()
     private var searchedMealLiveData = MutableLiveData<List<Meal>>()
 
+    // Save app state
+    private var saveStateRandomMeal: Meal? = null
     fun getRandomMeal(){
+        saveStateRandomMeal?.let {randomMeal ->
+            randomMealLiveData.postValue(randomMeal)
+            return
+        }
         RetrofitInstance.api.getRandomMeal().enqueue(object : Callback<MealList> {
             override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
                 if (response.body() != null){
                     val randomMeal: Meal = response.body()!!.meals[0]
                     randomMealLiveData.value = randomMeal
+                    saveStateRandomMeal = randomMeal
                 }else{
                     return
                 }
